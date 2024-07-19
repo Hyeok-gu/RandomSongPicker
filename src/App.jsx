3b6d763fcb68e1c1c1124a63d5055705bd15551c
DESKTOP-6I7R7M2\INUC_DESIGN_01 <thanatos6737@naver.com>
2024-07-19 09:04:57 +0900

    빌드 및 styled-components 설치

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Text = styled.p`
+  color: blue;
+`;
+
+function App() {
+  return (
+    <>
+      <Text>hello</Text>
+    </>
+  );
+}
+
+export default App;
